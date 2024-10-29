838fd348bea91fa2e446e3627b3835c243371105
aakash <aakashchaudhary172m@gmail.com>
2024-10-29 23:13:52 +0530

    setup frontend

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Header from "../component/Header";
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            Home page
+        </div>
+    )
+}
+export default Home;
